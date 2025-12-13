262ec5db69cefc55812885f894eb597bf50fd91c
Kyle Spaans <kyle@spaans.ca>
2025-12-13 17:23:14 -0500

    feat(puzzle): solve Day 8 Puzzle 1

--- 8/solve.js
+++ 8/solve.js
@@ -1,6 +1,6 @@
 export const TESTS = [
   { filename: 'test.txt', answer: 40},
-  { filename: 'input.txt', answer: 98698},
+  { filename: 'input.txt'},
 ]
 
 export const solve = (s, t) => {
@@ -43,37 +43,24 @@ export const solve = (s, t) => {
       }
     }
   }
-  console.log(` number of pairs: ${Object.keys(distance_pairs).length}`)
   const sorted_pairs = []
   for (const k in distance_pairs) {
     sorted_pairs.push([distance_pairs[k], k])
   }
   sorted_pairs.sort((a,b) => a[0]-b[0])
-  //l(sorted_pairs.slice(0,10))
+  l(sorted_pairs.slice(0,Math.floor(rows.length/2)))
   const edges = {}
   let circuit_sets = {}
   let circuits = 0
-  let ps
-  if (t) {
-    ps = sorted_pairs
-    //ps = sorted_pairs.slice(0,10)
-  } else {
-    ps = sorted_pairs
-  }
-  for (const p of ps) {
+  for (const p of sorted_pairs.slice(0,1000)) {
     const [a, b] = p[1].split(':')
     // build the graph in both directions, so that we don't duplicate edges
     if (edges[a] && edges[a].e.indexOf(b) === -1 &&
         edges[b] && edges[b].e.indexOf(a) === -1) {
-      l(`both nodes ${a}|${b} are already in circuits ${edges[a].circuit}|${edges[b].circuit}, adding edges`)
       if (edges[a].circuit !== edges[b].circuit) {
-        l(` gotta combine two circuits!`)
         const circuit_to_remove = edges[b].circuit
         for (const k in edges) {
           if (edges[k].circuit === circuit_to_remove) {
-            l(` setting edge  ${k} to be in circuit ${edges[a].circuit}`)
-            l(` removing node ${k} from circuit ${circuit_to_remove}`)
-            l(`-----`)
             circuit_sets[edges[a].circuit].add(k)
             circuit_sets[circuit_to_remove].delete(k)
             edges[k].circuit = edges[a].circuit
@@ -84,24 +71,20 @@ export const solve = (s, t) => {
         edges[b].e.push(a)
       }
     } else if (edges[a]) {
-      l(`node ${a} is already in a circuit, adding ${b} to circuit ${edges[a].circuit}`)
       edges[b] = { circuit: edges[a].circuit, e: [a] }
       edges[a].e.push(b)
       circuit_sets[edges[a].circuit].add(b)
     } else if (edges[b]) {
-      l(`node ${b} is already in a circuit, adding ${a} to circuit ${edges[b].circuit}`)
       edges[a] = { circuit: edges[b].circuit, e: [b] }
       edges[b].e.push(a)
       circuit_sets[edges[a].circuit].add(a)
     } else {
-      l(`new circuit ${circuits}: ${a}|${b}`)
       edges[a] = { circuit: circuits, e: [b] }
       edges[b] = { circuit: circuits, e: [a] }
       circuit_sets[circuits] = new Set([a,b])
       circuits += 1
     }
   }
-  l(edges)
   l(circuit_sets)
 
   const three_longest = []
